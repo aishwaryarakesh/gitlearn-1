65fd858251bdf89565c2125560aa20a1dd010037
Aiswarya Sarath <50009404+aishwaryarakesh@users.noreply.github.com>
2021-10-16 13:27:04 -0500

    Update all_checks.py

--- all_checks.py
+++ all_checks.py
@@ -7,14 +7,14 @@ def check_reboot();
 	"""Returns true if the computer has a pending reboot"""
 	return os.path.exists("/run/reboot-required")
 
-def check_disk_full(disk, min_absolute, min_percent):
+def check_disk_full(disk, min_abs, min_percent):
 	"""Returns True if there is not enough disk space"""
 	du = shutil.disk_usage(disk)
 	#Calculate the percent of free space
 	percent_free = 100 * du.free / du.total
 	#Calculate how manu free gigabytes
 	gigabytes_free = du.free /2**30
-	if percent_free < min_percent or gigabytes_free < min_absolute:
+	if percent_free < min_percent or gigabytes_free < min_abs:
 		return True
 	return False
 
